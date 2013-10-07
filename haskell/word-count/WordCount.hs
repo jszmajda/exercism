bb0dbadcdcce67c7909fd1807866e4905c669985
Joshua Szmajda <josh@optoro.com>
2013-10-06 21:06:56 -0400

    improved Map import for clarity

--- haskell/word-count/WordCount.hs
+++ haskell/word-count/WordCount.hs
@@ -2,12 +2,12 @@ module WordCount (wordCount) where
 
 import Data.Char
 import Data.List
-import qualified Data.Map as Map
+import Data.Map (Map, fromList)
 
-wordCount :: String -> Map.Map String Int
-wordCount s = Map.fromList $ map counts wordCollection
+wordCount :: String -> Map String Int
+wordCount message = fromList $ map counts wordCollection
   where
     counts list    = (head list, length list)
     wordCollection = group $ sort $ words lowers
-    lowers         = map sanitize s
-    sanitize c     = if (not $ isAlphaNum c) then ' ' else toLower c
+    lowers         = map sanitize message
+    sanitize c     = if (not . isAlphaNum) c then ' ' else toLower c
