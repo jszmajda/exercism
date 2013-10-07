2ead1404aae075c0c2377d7cc35c7971352110ad
Joshua Szmajda <josh@optoro.com>
2013-10-06 22:41:01 -0400

    added alternate implementation

--- haskell/word-count/WordCount.hs
+++ haskell/word-count/WordCount.hs
@@ -1,13 +1,21 @@
 module WordCount (wordCount) where
 
 import Data.Char
-import Data.List
-import Data.Map (Map, fromList)
+import qualified Data.Map as M
 
-wordCount :: String -> Map String Int
-wordCount message = fromList $ map counts wordCollection
+wordCount :: String -> M.Map String Int
+wordCount message = foldl collate M.empty wordCollection
   where
-    counts list    = (head list, length list)
-    wordCollection = group $ sort $ words lowers
-    lowers         = map sanitize message
-    sanitize c     = if (not . isAlphaNum) c then ' ' else toLower c
+    collate set k   = M.insert k (countUp set k) set
+    countUp set k   = (M.findWithDefault 0 k set) + 1
+    wordCollection  = words lowers
+    lowers          = map sanitize message
+    sanitize c      = if (not . isAlphaNum) c then ' ' else toLower c
+
+-- wordCount :: String -> Map String Int
+-- wordCount message = fromList $ map counts wordCollection
+--   where
+--     counts list    = (head list, length list)
+--     wordCollection = group $ sort $ words lowers
+--     lowers         = map sanitize message
+--     sanitize c     = if (not . isAlphaNum) c then ' ' else toLower c
