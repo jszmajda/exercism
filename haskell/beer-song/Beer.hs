334b98319f30fc7d71aad5431e73c0d14b0f9abc
Joshua Szmajda <josh@optoro.com>
2013-10-23 15:15:52 -0400

    unwords is great

--- haskell/beer-song/Beer.hs
+++ haskell/beer-song/Beer.hs
@@ -1,23 +1,25 @@
-module Beer where
+module Beer (verse, sing) where
 
 import Data.Char (toUpper)
+
+initialCaps :: String -> String
+initialCaps []     = []
+initialCaps (x:xs) = toUpper x : xs
 
 verse :: Int -> String
 verse n = firstPart ++ secondPart
   where
-    firstPart  = initialCaps s ++ " " ++ bottle n ++ " of beer on the wall, " ++ s ++ " " ++ bottle n ++ " of beer.\n"
-    secondPart = whatToDo n ++ ", " ++ sh (n-1) ++ " " ++ bottle (n - 1) ++ " of beer on the wall.\n"
-    whatToDo 0 = "Go to the store and buy some more"
-    whatToDo _ = "Take " ++ one n ++ " down and pass it around"
-    initialCaps (x:xs) = toUpper x : xs
-    bottle 1 = "bottle"
-    bottle _ = "bottles"
-    one 1 = "it"
-    one _ = "one"
-    s = sh n
-    sh 0 = "no more"
-    sh (-1) = "99"
-    sh x = show x
+    firstPart  = unwords [initialCaps (sh n), bottle n, "of beer on the wall,", sh n, bottle n, "of beer.\n"]
+    secondPart = unwords [whatToDo n, sh (n-1), bottle (n - 1), "of beer on the wall.\n"]
+    whatToDo 0 = "Go to the store and buy some more,"
+    whatToDo _ = unwords ["Take", one n, "down and pass it around,"]
+    bottle 1   = "bottle"
+    bottle _   = "bottles"
+    one 1      = "it"
+    one _      = "one"
+    sh 0       = "no more"
+    sh (-1)    = "99"
+    sh x       = show x
 
 sing :: Int -> Int -> String
 sing from to = unlines $ map verse $ list from to
