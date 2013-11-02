d2b7a91fe7e2bdb4315019d2fdaae144d587282d
Joshua Szmajda <josh@optoro.com>
2013-11-02 12:52:22 -0400

    Refactorings from suggestions

--- haskell/phone-number/Phone.hs
+++ haskell/phone-number/Phone.hs
@@ -6,9 +6,11 @@ number :: String -> String
 number n | valid     = lastTen
          | otherwise = "0000000000"
   where
-    valid   = length digits == 10 || (length digits == 11 && head digits == '1')
-    lastTen = reverse $ take 10 $ reverse digits
+    valid   = len == 10 || (len == 11 && head digits == '1')
+    lastTen = drop extra digits
     digits  = filter isNumber n
+    len     = length digits
+    extra   = len - 10
 
 areaCode :: String -> String
 areaCode = take 3
@@ -18,5 +20,5 @@ prettyPrint s = "(" ++ (areaCode n) ++ ") " ++ localCode ++ "-" ++ lastFour
   where
     n         = number s
     localCode = slice 3 5
-    lastFour  = slice 6 9
+    lastFour  = drop 6 n
     slice f t = take (t - f + 1) (drop f n)
