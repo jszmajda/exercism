c14a7527460dd2e57e83976222034c74bad52fba
Joshua Szmajda <josh@optoro.com>
2013-11-02 15:41:39 -0400

    refactoring suggested by etrepum

--- haskell/phone-number/Phone.hs
+++ haskell/phone-number/Phone.hs
@@ -19,6 +19,6 @@ prettyPrint :: String -> String
 prettyPrint s = "(" ++ (areaCode n) ++ ") " ++ localCode ++ "-" ++ lastFour
   where
     n         = number s
-    localCode = slice 3 5
-    lastFour  = drop 6 n
-    slice f t = take (t - f + 1) (drop f n)
+    localCode = fst parts
+    lastFour  = snd parts
+    parts     = splitAt 3 $ drop 3 n
