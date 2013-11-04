cc871ee4299ff83b4bf44f04a104d0576e8b96ed
Joshua Szmajda <josh@optoro.com>
2013-11-04 11:19:23 -0500

    Cleaned up, better understanding of . vs $

--- haskell/grade-school/School.hs
+++ haskell/grade-school/School.hs
@@ -3,16 +3,16 @@ module School (School(..), add, empty, sorted, grade) where
 import Data.List (sort)
 import qualified Data.IntMap.Strict as M
 
-data School = Sch { schoolData :: ( M.IntMap [String] ) }
+data School = Sch { schoolData :: M.IntMap [String] }
 
 empty :: School
-empty = Sch $ M.empty
+empty = Sch M.empty
 
 add :: Int -> String -> School -> School
-add g n s = Sch $ M.insertWith (++) g [n] $ schoolData s
+add g n = Sch . M.insertWith (++) g [n] . schoolData
 
 sorted :: School -> [(Int, [String])]
-sorted s = M.toAscList $ M.map (sort) $ schoolData s
+sorted = M.toAscList . M.map sort . schoolData
 
 grade :: Int -> School -> [String]
-grade g s = M.findWithDefault [] g $ schoolData s
+grade g = M.findWithDefault [] g . schoolData
