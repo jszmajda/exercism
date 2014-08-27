f056b4e8f00d19bf357177309e2e3a5754d22e2c
Joshua Szmajda <josh@optoro.com>
2014-08-26 20:04:06 -0400

    wow, mapM is awesome

--- haskell/robot-name/Robot.hs
+++ haskell/robot-name/Robot.hs
@@ -10,29 +10,16 @@ robotName r = readIORef $ name r
 
 mkRobot :: IO Robot
 mkRobot = do
-  name <- generateName
-  nm <- newIORef name
+  nm <- newIORef =<< generateName
   return $ Robot nm
 
 resetName :: Robot -> IO ()
 resetName r = do
-  nm <- generateName
-  writeIORef (name r) nm
+  writeIORef (name r) =<< generateName
   return ()
 
 generateName :: IO String
-generateName = do
-  g1 <- getStdGen
-  let r1 = randomR ('A','Z') g1
-  let c1 = fst r1
-  let r2 = randomR ('A','Z') (snd r1)
-  let c2 = fst r2
-  let r3 = randomR (0,9) (snd r2) :: (Int, StdGen)
-  let n1 = fst r3
-  let r4 = randomR (0,9) (snd r3) :: (Int, StdGen)
-  let n2 = fst r4
-  let r5 = randomR (0,9) (snd r4) :: (Int, StdGen)
-  let n3 = fst r5
-  let nm = (c1:c2: (foldl (++) "" (map show [n1,n2,n3])))
-  setStdGen (snd r5)
-  return nm
+generateName = mapM randomRIO [c,c,n,n,n]
+  where
+    c = ('A','Z')
+    n = ('0','9')
