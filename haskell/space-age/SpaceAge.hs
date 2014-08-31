eb6936b9bf16b6bfce29a611d7a1fe030f646168
Joshua Szmajda <josh@optoro.com>
2014-08-31 18:54:52 -0400

    pointfree style

--- haskell/space-age/SpaceAge.hs
+++ haskell/space-age/SpaceAge.hs
@@ -4,13 +4,6 @@ data Planet =
   Earth | Mercury | Venus | Mars | Jupiter | Saturn | Uranus | Neptune
 
 ageOn :: Planet -> Float -> Float
--- ageOn planet seconds = seconds / yearLength planet
--- ageOn = (/) . yearLength
--- ageOn planet = (/ yearLength planet)
--- ageOn        = flip (/) . yearLength
--- ageOn planet seconds = (1 / yearLength planet) * seconds
--- ageOn planet = (*) (1 / yearLength planet)
--- ageOn = (*) . (1 /) . yearLength
 ageOn = divideBy . yearLength
   where divideBy = (*) . (1 /)
 
