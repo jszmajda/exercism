4bbdd6021ae9b1a18108d3013dc13b27b369d3fc
Joshua Szmajda <josh@optoro.com>
2014-08-31 16:56:12 -0400

    refactored for clarity

--- haskell/gigasecond/Gigasecond.hs
+++ haskell/gigasecond/Gigasecond.hs
@@ -1,10 +1,8 @@
 module Gigasecond (fromDay) where
+
 import Data.Time.Calendar (Day, addDays)
 
 fromDay :: Day -> Day
-fromDay = addDays $ round gigadays
+fromDay = addDays $ round oneGigaday
   where
-    gigadays    = gigahours / 24
-    gigahours   = gigaminutes / 60
-    gigaminutes = gs / 60
-    gs          = 10 ^ 9
+    oneGigaday = 10 ^ 9 / 86400
---
+++ haskell/gigasecond/doit.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "---------------------"
+runhaskell gigasecond_test.hs
