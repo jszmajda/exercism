4518ed91b1d27e30ecd041a17060875de8c02492
Joshua Szmajda <josh@optoro.com>
2014-08-31 18:25:35 -0400

    this pointfree refactoring makes much more sense to me

--- haskell/sum-of-multiples/SumOfMultiples.hs
+++ haskell/sum-of-multiples/SumOfMultiples.hs
@@ -3,11 +3,10 @@ module SumOfMultiples (sumOfMultiples, sumOfMultiplesDefault) where
 sumOfMultiples :: [Integer] -> Integer -> Integer
 sumOfMultiples factors limit = sum multiples
   where
-    multiples    = filter isMultiple [1..(limit - 1)]
-    isMultiple i = any' $ (0 ==) . mod i
-    any'         = flip any factors
-    -- this pointfree alternative is confusing to read IMO
-    -- isMultiple = any' . ((0 ==) .) . mod
+    multiples  = filter isMultiple [1..(limit - 1)]
+    isMultiple = (`any` factors) . ((0 ==) .: mod)
 
 sumOfMultiplesDefault :: Integer -> Integer
 sumOfMultiplesDefault = sumOfMultiples [3,5]
+
+f .: g = \x y -> f (g x y)
