868f3895ac76b7390664e4636ba73f3dd0953d20
Joshua Szmajda <josh@optoro.com>
2014-08-31 23:21:09 -0400

    all passes, needs refactoring

--- haskell/list-ops/ListOps.hs
+++ haskell/list-ops/ListOps.hs
@@ -13,25 +13,34 @@ import Prelude hiding
   ( length, reverse, map, filter, foldr, (++), concat )
 
 foldl' :: (b -> a -> b) -> b -> [a] -> b
-foldl' = undefined
+foldl' _ s []     = s
+foldl' f s (x:xs) = foldl' f (f (seq s s) x) xs
 
 foldr :: (a -> b -> b) -> b -> [a] -> b
-foldr = undefined
+foldr _ s [] = s
+foldr f s (x:xs) = f x (foldr f s xs)
 
 length :: [a] -> Int
-length = undefined
+length [] = 0
+length (x:xs) = 1 + length xs
 
 reverse :: [a] -> [a]
-reverse = undefined
+reverse []     = []
+reverse (x:xs) = reverse xs ++ [x]
 
 map :: (a -> b) -> [a] -> [b]
-map = undefined
+map f [] = []
+map f (x:xs) = f x : map f xs
 
 filter :: (a -> Bool) -> [a] -> [a]
-filter = undefined
+filter f []     = []
+filter f (x:xs) | f x       = x : filter f xs
+                | otherwise =     filter f xs
 
 (++) :: [a] -> [a] -> [a]
-xs ++ ys = undefined
+xs ++ [] = xs
+[] ++ ys = ys
+(x:xs) ++ ys = x : (xs ++ ys)
 
 concat :: [[a]] -> [a]
-concat = undefined
+concat = foldr (++) []
