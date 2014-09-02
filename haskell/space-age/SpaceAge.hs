dcb7ceccc65705e7d1e65e0b4674fab42a767c30
Joshua Szmajda <josh@optoro.com>
2014-09-01 21:29:42 -0400

    refactored for more clarity

--- haskell/space-age/SpaceAge.hs
+++ haskell/space-age/SpaceAge.hs
@@ -6,22 +6,19 @@ data Planet =
   Mercury | Venus | Earth | Mars | Jupiter | Saturn | Uranus | Neptune
 
 ageOn :: Planet -> Float -> Float
-ageOn = divideBy . planetSeconds
-  where
-    divideBy      = (*) . (1 /)
-    planetSeconds = (earthSeconds *) . yearLength
+ageOn = divideBy . yearLength
+  where divideBy = (*) . (1 /)
 
 yearLength :: Planet -> Float
-yearLength p =
-  case p of
-    Earth   -> 1.0
-    Mercury -> 0.2408467
-    Venus   -> 0.61519726
-    Mars    -> 1.8808158
-    Jupiter -> 11.862615
-    Saturn  -> 29.447498
-    Uranus  -> 84.016846
-    Neptune -> 164.79132
+yearLength p = earthSeconds * case p of
+  Earth   -> 1.0
+  Mercury -> 0.2408467
+  Venus   -> 0.61519726
+  Mars    -> 1.8808158
+  Jupiter -> 11.862615
+  Saturn  -> 29.447498
+  Uranus  -> 84.016846
+  Neptune -> 164.79132
 
 earthSeconds :: Float
 earthSeconds = 31557600.0
