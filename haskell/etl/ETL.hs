80b3970495b532605070878c2c0a79ec900d6d4f
Joshua Szmajda <josh@optoro.com>
2014-09-02 16:08:51 -0400

    working solution

--- haskell/etl/ETL.hs
+++ haskell/etl/ETL.hs
@@ -1,5 +1,12 @@
 module ETL (transform) where
+
 import qualified Data.Map as M
+import Data.Char (toLower)
 
 transform :: M.Map Int [String] -> M.Map String Int
-transform = undefined
+transform = M.fromList . remap . M.toList
+  where remap = concatMap unwindPair
+
+unwindPair :: (Int, [String]) -> [(String, Int)]
+unwindPair (score, letters) = map (\ l -> (tl l, score)) letters
+  where tl = map toLower
