b98244e46e5e6563c4be1f22984811b9089e8e14
Joshua Szmajda <josh@optoro.com>
2014-09-09 12:45:18 -0400

    removed shadowing warning

--- haskell/etl/ETL.hs
+++ haskell/etl/ETL.hs
@@ -9,7 +9,7 @@ transform = fst . M.mapAccumWithKey accum base
   where base = M.empty :: M.Map String Int
 
 accum :: M.Map String Int -> Int -> [String] -> (M.Map String Int, Int)
-accum z score letters = (foldInsert, score)
+accum acc score letters = (foldInsert, score)
   where
-    foldInsert = foldl' (\ z l -> M.insert (tl l) score z) z letters
+    foldInsert = foldl' (\ z l -> M.insert (tl l) score z) acc letters
     tl         = map toLower
