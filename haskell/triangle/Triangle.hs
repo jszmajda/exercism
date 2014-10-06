99c6e3ec1ae91eb432cca6cb754ead775e6eb1cb
Joshua Szmajda <josh@optoro.com>
2014-10-05 22:23:06 -0400

    fixed math error

--- haskell/triangle/Triangle.hs
+++ haskell/triangle/Triangle.hs
@@ -1,7 +1,25 @@
 module Triangle (TriangleType(..), triangleType) where
 
+import Data.List
+import Debug.Trace
+
 data TriangleType = Equilateral | Isosceles | Scalene | Illogical
   deriving (Show, Eq)
 
+-- The sum of the lengths of any two sides of a triangle always exceeds the
+-- length of the third side, a principle known as the _triangle
+-- inequality_.
 triangleType :: Int -> Int -> Int -> TriangleType
-triangleType a b c = Equilateral
+triangleType a b c
+  | invalid                    = Illogical
+  | a == b && a == c           = Equilateral
+  | a /= b && a /= c && b /= c = Scalene
+  | a == b || a == c || b == c = Isosceles
+  | otherwise                  = Illogical
+  where
+    invalid = any (\[x,y,z] -> x >= (y + z)) sideCombos
+    sideCombos = permutations asList
+    asList = [a,b,c]
+
+traceShowId :: (Show a) => a -> a
+traceShowId a = trace (show a) a
