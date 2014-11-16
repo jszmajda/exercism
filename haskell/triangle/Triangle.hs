25a3aa5403618fbfd87ab19ca3c6d40264125f66
Joshua Szmajda <josh@optoro.com>
2014-11-16 15:09:18 -0500

    Passing tests

--- haskell/triangle/Triangle.hs
+++ haskell/triangle/Triangle.hs
@@ -1,7 +1,21 @@
 module Triangle (TriangleType(..), triangleType) where
 
+import Data.List
+
 data TriangleType = Equilateral | Isosceles | Scalene | Illogical
   deriving (Show, Eq)
 
+-- The sum of the lengths of any two sides of a triangle always exceeds the
+-- length of the third side, a principle known as the _triangle
+-- inequality_.
 triangleType :: Int -> Int -> Int -> TriangleType
-triangleType a b c = Equilateral
+triangleType a b c
+  | invalid                    = Illogical
+  | a == b && a == c           = Equilateral
+  | a /= b && a /= c && b /= c = Scalene
+  | a == b || a == c || b == c = Isosceles
+  | otherwise                  = Illogical -- exhausting patterns
+  where
+    invalid = any (\[x,y,z] -> x >= (y + z)) sideCombos
+    sideCombos = permutations asList
+    asList = [a,b,c]
