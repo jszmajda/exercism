059a3a37576ad18532e6eafdd7bff15950217440
Joshua Szmajda <josh@optoro.com>
2014-11-22 20:07:53 -0500

    a little quicker to check the or conditions

--- haskell/triangle/Triangle.hs
+++ haskell/triangle/Triangle.hs
@@ -12,8 +12,8 @@ triangleType :: Int -> Int -> Int -> TriangleType
 triangleType a b c
   | invalid                    = Illogical
   | a == b && a == c           = Equilateral
-  | a /= b && a /= c && b /= c = Scalene
   | a == b || a == c || b == c = Isosceles
+  | a /= b && a /= c && b /= c = Scalene
   | otherwise                  = Illogical -- exhausting patterns
   where
     invalid = any (\[x,y,z] -> x >= (y + z)) sideCombos
