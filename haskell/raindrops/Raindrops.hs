25d6cbff9d1252e74c57af5330b70b87415d31fc
Joshua Szmajda <josh@optoro.com>
2014-11-23 22:49:26 -0500

    it works

--- haskell/raindrops/Raindrops.hs
+++ haskell/raindrops/Raindrops.hs
@@ -1,3 +1,23 @@
 module Raindrops (convert) where
 
-convert = undefined
+import Data.List
+import Data.Maybe (maybeToList)
+
+convert :: Int -> String
+convert n  = convertFacts (facts n) n
+
+convertFacts :: [Int] -> Int -> String
+convertFacts [] n = show n
+convertFacts fs _ = concatMap toDrop fs
+  where
+    toDrop 3 = "Pling"
+    toDrop 5 = "Plang"
+    toDrop _ = "Plong"
+
+facts :: Int -> [Int]
+facts n = nub $ maybeToList factor ++ rest factor
+  where
+    rest (Just r) = facts (n `div` r)
+    rest Nothing = []
+    factor = find (\p -> n `mod` p == 0) primes
+    primes = [3,5,7]
