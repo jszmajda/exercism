7f790e98997d57f4d26b79c8cdf25196e65afe95
Joshua Szmajda <josh@optoro.com>
2014-11-25 20:26:47 -0500

    refactored to list the cases out

--- haskell/raindrops/Raindrops.hs
+++ haskell/raindrops/Raindrops.hs
@@ -1,23 +1,13 @@
 module Raindrops (convert) where
 
-import Data.List
-import Data.Maybe (maybeToList)
-
 convert :: Int -> String
-convert n  = convertFacts (facts n) n
-
-convertFacts :: [Int] -> Int -> String
-convertFacts [] n = show n
-convertFacts fs _ = concatMap toDrop fs
+convert n | fact 105 = "PlingPlangPlong"
+          | fact 35  = "PlangPlong"
+          | fact 21  = "PlingPlong"
+          | fact 15  = "PlingPlang"
+          | fact 7   = "Plong"
+          | fact 5   = "Plang"
+          | fact 3   = "Pling"
+          | otherwise = show n
   where
-    toDrop 3 = "Pling"
-    toDrop 5 = "Plang"
-    toDrop _ = "Plong"
-
-facts :: Int -> [Int]
-facts n = nub $ maybeToList factor ++ rest factor
-  where
-    rest (Just r) = facts (n `div` r)
-    rest Nothing = []
-    factor = find (\p -> n `mod` p == 0) primes
-    primes = [3,5,7]
+    fact = (== 0) . mod n
