682ae309ac9e65b32da578c5b20d582232940186
Joshua Szmajda <josh@optoro.com>
2014-11-25 20:32:08 -0500

    refactored again to not hardcode factors

--- haskell/raindrops/Raindrops.hs
+++ haskell/raindrops/Raindrops.hs
@@ -1,13 +1,11 @@
 module Raindrops (convert) where
 
 convert :: Int -> String
-convert n | fact 105 = "PlingPlangPlong"
-          | fact 35  = "PlangPlong"
-          | fact 21  = "PlingPlong"
-          | fact 15  = "PlingPlang"
-          | fact 7   = "Plong"
-          | fact 5   = "Plang"
-          | fact 3   = "Pling"
-          | otherwise = show n
+convert n = if (not . null) factors then showFactors else show n
   where
-    fact = (== 0) . mod n
+    factors      = filter isFactor [3,5,7]
+    isFactor     = (== 0) . mod n
+    showFactors  = concatMap showFactor factors
+    showFactor 3 = "Pling"
+    showFactor 5 = "Plang"
+    showFactor _ = "Plong"
