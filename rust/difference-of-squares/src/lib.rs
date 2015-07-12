41975bf0c46a8df358deac03a77efb33a33f4299
Joshua Szmajda <josh@optoro.com>
2015-07-12 18:36:46 -0400

    solved pretty basically

--- rust/difference-of-squares/src/lib.rs
+++ rust/difference-of-squares/src/lib.rs
@@ -1,12 +1,16 @@
 pub fn sum_of_squares(n: u32) -> u32 {
     let mut x = 0;
-    for i in (0..n) {
-        x += i ^ 2;
+    for i in (1..n + 1) {
+        x += i.pow(2)
     }
     x
 }
 pub fn square_of_sum(n: u32) -> u32 {
-    n
+    let mut x = 0;
+    for i in (1..n + 1) {
+        x += i
+    }
+    x.pow(2)
 }
 
 pub fn difference(n: u32) -> u32 {
