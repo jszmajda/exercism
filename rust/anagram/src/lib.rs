61edef433f39b11fc485b65d5701cc74523f12a0
Joshua Szmajda <josh@optoro.com>
2015-07-12 16:17:04 -0400

    updates from nitpicks

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,28 +1,29 @@
 pub fn anagrams_for<'a> (source: &str, candidates: &'a [&str]) -> Vec<&'a str> {
-    // not really sure why I need this map in the pipeline
-    candidates.into_iter().map(|e| *e ).filter( |example| {
-        is_anagram_of(source, *example)
-    }).collect::<Vec<&str>>()
+    candidates.iter()
+              .map(AsRef::as_ref) // equiv .map({|e| *e })
+              .filter( |example| { is_anagram_of(source, *example) })
+              .collect()
 }
 
 
 // sorts string bytes and compares the result. It might be faster to walk each string once, build
 // some data structure, and compare that but I'm not braining well today.
 fn is_anagram_of(source: &str, test: &str) -> bool {
-    let lowered_source = lower(source.to_string());
-    let lowered_test   = lower(test.to_string());
-    if lowered_source == lowered_test {
-        return false
-    } else {
-        let mut sb = lowered_source.into_bytes();
-        let mut tb = lowered_test.into_bytes();
-        sb.sort_by(|a,b| a.cmp(b));
-        tb.sort_by(|a,b| a.cmp(b));
-        sb == tb
-    }
+    let lowered_source = upper(source);
+    let lowered_test   = upper(test);
+
+    if lowered_source == lowered_test { return false; }
+
+    let mut sb: Vec<char> = lowered_source.chars().collect();
+    let mut tb: Vec<char> = lowered_test.chars().collect();
+    sb.sort();
+    tb.sort();
+    sb == tb
 }
 
-fn lower(str: String) -> String {
-    // good god this is hideous
-    str.chars().into_iter().map(|c| c.to_lowercase().next().unwrap() ).collect::<String>()
+// this is basically Rust 1.2's String::to_uppercase
+fn upper(s: &str) -> String {
+    let mut upper_string = String::with_capacity(s.len());
+    upper_string.extend(s.chars().flat_map(|c| c.to_uppercase()));
+    return upper_string;
 }
