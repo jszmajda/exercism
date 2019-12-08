c9715bf8aae901411f6aec8664596a1d84d92206
Jess Szmajda <jess@optoro.com>
2019-12-08 12:16:22 -0500

    refactored based on learning from community solutions

--- haskell/perfect-numbers/src/PerfectNumbers.hs
+++ haskell/perfect-numbers/src/PerfectNumbers.hs
@@ -4,12 +4,9 @@ data Classification = Deficient | Perfect | Abundant deriving (Eq, Show)
 
 classify :: Int -> Maybe Classification
 classify n
-  | n <= 0    = Nothing
-  | otherwise = Just $ classify' sumOfFactors
+  | n <= 0          = Nothing
+  | aliquotSum == n = Just Perfect
+  | aliquotSum < n  = Just Deficient
+  | otherwise       = Just Abundant
   where
-    sumOfFactors = foldl (+) 0 factors
-    factors = filter ((0 ==) . mod n) [1..(n `div` 2)]
-    classify' a
-      | a == n    = Perfect
-      | a > n     = Abundant
-      | otherwise = Deficient
+    aliquotSum = sum $ filter ((0 ==) . mod n) [1..(n `div` 2)]
