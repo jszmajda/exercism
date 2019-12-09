08268fa8b1ea435bfb17bedf80459c1a64673ce8
Jess Szmajda <jess@optoro.com>
2019-12-09 17:37:51 -0500

    I feel like this is inelegant but it works

--- haskell/isogram/src/Isogram.hs
+++ haskell/isogram/src/Isogram.hs
@@ -1,4 +1,12 @@
 module Isogram (isIsogram) where
 
+import qualified Data.Text as T
+import Data.List (sort)
+
 isIsogram :: String -> Bool
-isIsogram = error "You need to implement this function!"
+isIsogram t = not $ any (\g -> T.length g > 1) grouped
+  where
+    grouped   = T.group filtered
+    filtered  = T.filter (\c -> c `elem` ['a'..'z']) downcased
+    downcased = T.toLower sorted
+    sorted    = T.pack $ sort t
