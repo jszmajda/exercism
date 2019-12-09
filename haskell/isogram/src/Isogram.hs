c7b73984cfd4f87bdbbd2c58cf5ab2ec2c729679
Jess Szmajda <jess@optoro.com>
2019-12-09 17:42:38 -0500

    added isAlpha

--- haskell/isogram/src/Isogram.hs
+++ haskell/isogram/src/Isogram.hs
@@ -2,11 +2,12 @@ module Isogram (isIsogram) where
 
 import qualified Data.Text as T
 import Data.List (sort)
+import Data.Char (isAlpha)
 
 isIsogram :: String -> Bool
 isIsogram t = not $ any (\g -> T.length g > 1) grouped
   where
     grouped   = T.group filtered
-    filtered  = T.filter (\c -> c `elem` ['a'..'z']) downcased
+    filtered  = T.filter isAlpha downcased
     downcased = T.toLower sorted
     sorted    = T.pack $ sort t
