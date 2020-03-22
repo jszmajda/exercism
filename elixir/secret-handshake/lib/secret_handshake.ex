47420cd50fb93d923f1586ca9c051c66ad7d354a
Jess Szmajda <jess.szmajda@axios.com>
2020-03-22 12:41:20 -0400

    gnarly but eh

--- elixir/secret-handshake/lib/secret_handshake.ex
+++ elixir/secret-handshake/lib/secret_handshake.ex
@@ -1,4 +1,8 @@
+use Bitwise
+
 defmodule SecretHandshake do
+  @operations ["wink", "double blink", "close your eyes", "jump"]
+
   @doc """
   Determine the actions of a secret handshake based on the binary
   representation of the given `code`.
@@ -11,9 +15,23 @@ defmodule SecretHandshake do
   100 = close your eyes
   1000 = jump
 
-  10000 = Reverse the order of the operations in the secret handshake
+  10000 = Reverse the order of the operat ions in the secret handshake
   """
   @spec commands(code :: integer) :: list(String.t())
   def commands(code) do
+    Enum.with_index(@operations)
+    |> List.foldl([], fn tup, acc ->
+      {e, i} = tup
+      check = 0b1 <<< i
+
+      if (code &&& check) > 0, do: acc ++ [e], else: acc
+    end)
+    |> (fn list ->
+          if (code &&& 0b10000) > 0 do
+            Enum.reverse(list)
+          else
+            list
+          end
+        end).()
   end
 end
