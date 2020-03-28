e2ff6ab09a49cccef612c0dd518cc2d66c3b7920
Jess Szmajda <jess.szmajda@axios.com>
2020-03-27 21:15:05 -0400

    ok, all work now

--- elixir/roman-numerals/lib/roman_numerals.ex
+++ elixir/roman-numerals/lib/roman_numerals.ex
@@ -1,33 +1,39 @@
 defmodule RomanNumerals do
-  @digits [
-    {"M", 1000},
-    {"D", 500},
-    {"C", 100},
-    {"L", 50},
-    {"X", 10},
-    {"V", 5},
-    {"I", 1}
-  ]
   @doc """
   Convert the number to a roman number.
   """
   @spec numeral(pos_integer) :: String.t()
   def numeral(number) do
-    @digits
-    |> Enum.reduce({number, ""}, fn digit_tuple, acc_tuple ->
-      {roman_numeral, numeral_value} = digit_tuple
-      {rem, roman} = acc_tuple
+    rem = number
+    {val, rem} = whatever(number, "", 1000, "", "M", "")
+    {val, rem} = whatever(rem, val, 100, "D", "C", "M")
+    {val, rem} = whatever(rem, val, 10, "L", "X", "C")
+    {val, rem} = whatever(rem, val, 1, "V", "I", "X")
+    val
+  end
 
-      {newnum, romo} =
-        if rem >= numeral_value do
-          mod = div(rem, numeral_value)
-          {rem - numeral_value * mod, String.duplicate(roman_numeral, mod) <> roman}
-        else
-          {rem, roman}
-        end
+  defp whatever(number, val, base, five, one, ten) do
+    base_times = div(number, base)
+    rem = rem(number, base)
 
-      {newnum, romo}
-    end)
-    |> elem(1)
+    append_val =
+      cond do
+        number >= 9 * base ->
+          one <> ten
+
+        number >= 5 * base ->
+          five <> String.duplicate(one, base_times - 5)
+
+        number >= 4 * base ->
+          one <> five
+
+        number >= base ->
+          String.duplicate(one, base_times)
+
+        true ->
+          ""
+      end
+
+    {val <> append_val, rem}
   end
 end
