46c2664c56fcecd7ee3a7065bdb825b41e366ad7
Jess Szmajda <jess.szmajda@axios.com>
2020-04-08 20:46:08 -0400

    it works now

--- elixir/protein-translation/lib/protein_translation.ex
+++ elixir/protein-translation/lib/protein_translation.ex
@@ -1,9 +1,43 @@
 defmodule ProteinTranslation do
+  @codons %{
+    "UGU" => "Cysteine",
+    "UGC" => "Cysteine",
+    "UUA" => "Leucine",
+    "UUG" => "Leucine",
+    "AUG" => "Methionine",
+    "UUU" => "Phenylalanine",
+    "UUC" => "Phenylalanine",
+    "UCU" => "Serine",
+    "UCC" => "Serine",
+    "UCA" => "Serine",
+    "UCG" => "Serine",
+    "UGG" => "Tryptophan",
+    "UAU" => "Tyrosine",
+    "UAC" => "Tyrosine",
+    "UAA" => "STOP",
+    "UAG" => "STOP",
+    "UGA" => "STOP"
+  }
+
   @doc """
   Given an RNA string, return a list of proteins specified by codons, in order.
   """
   @spec of_rna(String.t()) :: {atom, list(String.t())}
   def of_rna(rna) do
+    codons =
+      String.graphemes(rna)
+      |> Enum.chunk_every(3, 3)
+      |> Enum.map(&Enum.join(&1, ""))
+      |> Enum.map(&of_codon(&1))
+
+    if Enum.any?(codons, fn e -> elem(e, 0) == :error end) do
+      {:error, "invalid RNA"}
+    else
+      {:ok,
+       codons
+       |> Enum.take_while(fn {r, e} -> r == :ok && e != "STOP" end)
+       |> Enum.map(&Kernel.elem(&1, 1))}
+    end
   end
 
   @doc """
@@ -29,5 +63,8 @@ defmodule ProteinTranslation do
   """
   @spec of_codon(String.t()) :: {atom, String.t()}
   def of_codon(codon) do
+    if Map.has_key?(@codons, codon),
+      do: {:ok, @codons[codon]},
+      else: {:error, "invalid codon"}
   end
 end
