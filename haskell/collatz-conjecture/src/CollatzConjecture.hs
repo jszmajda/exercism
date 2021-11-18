28fcb53b4445d23887ebc2648079d73781c4deb5
Jess Szmajda <jess.szmajda@gmail.com>
2021-11-18 08:14:27 -0500

    small simplification with genericLength

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,9 +1,12 @@
 module CollatzConjecture (collatz) where
 
+-- `length` returns Int, `genericLength` coerces to any target Num
+import Data.List (genericLength) 
+
 collatz :: Integer -> Maybe Integer
 collatz int = if int < 1 then Nothing else Just $ result
   where
-    result = toInteger $ length $ takeWhile (/= 1) $ iterate collatzStep int
+    result = genericLength $ takeWhile (/= 1) $ iterate collatzStep int
 
 collatzStep :: Integer -> Integer
 collatzStep i = if (even i) then i `div` 2 else i * 3 + 1
